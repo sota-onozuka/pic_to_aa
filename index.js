57592e34156d67370f0789c925cbd70afebe4f7c
sota-onozuka <sonta.afcot37@gmail.com>
2021-07-10 15:03:00 +0900

    変数名を見直し

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const fs = require('fs')
 const Math = require('math')
 const Canvas = require('canvas')
-const path = require('path')
+const Path = require('path')
 const Image = Canvas.Image
 
-fs.readFile(path.join(`${process.cwd()}`, `/${process.argv[process.argv.length - 1]}`), function (err, data) {
+fs.readFile(Path.join(`${process.cwd()}`, `/${process.argv[process.argv.length - 1]}`), function (err, data) {
   if (err) throw err
 
   const img = new Image()
@@ -15,22 +15,22 @@ fs.readFile(path.join(`${process.cwd()}`, `/${process.argv[process.argv.length -
   const ctx = canvas.getContext('2d')
   ctx.drawImage(img, 0, 0, img.width, img.height)
 
-  const imagedata = ctx.getImageData(0, 0, img.width, img.height) // get list of gbr pixel values like [[g at [0, 0], [r at [0, 0], [b at [0, 0], [a at [0, 0], [g at [0, 1]...]
+  const imageData = ctx.getImageData(0, 0, img.width, img.height) // get list of gbr pixel values like [[g at [0, 0], [r at [0, 0], [b at [0, 0], [a at [0, 0], [g at [0, 1]...]
 
-  const resize = Math.floor(img.width / 60) // 倍率
-  const aaWidth = Math.floor(img.width / resize); // defines the width and height of the output
-  const aaHeight = Math.floor(img.height / resize)
-  const colorset = '縁蜜眠年日あやすかい＄？￥＝～｜；・。、　'
+  const compressionRate = Math.floor(img.width / 60)
+  const aaWidth = Math.floor(img.width / compressionRate); // defines the width and height of the output
+  const aaHeight = Math.floor(img.height / compressionRate)
+  const colorSet = '縁蜜眠年日あやすかい＄？￥＝～｜；・。、　' // expression symbols, you can change symbols
 
   let output = '\n'
   for (let y = 0; y < aaHeight; y++) {
     output += '\n'
     for (let x = 0; x < aaWidth; x++) {
-      const index = (y * imagedata.width + x) * 4 * resize
-      output += (colorset[Math.floor(colorset.length * (imagedata.data[index] +
-                imagedata.data[index + 1] + imagedata.data[index + 2] +
-                imagedata.data[index + 3]) / 4 / 256)]) // outputs an avarage of gbra pixels as translated string in colorset
-      if (typeof colorset[Math.floor(colorset.length * imagedata.data[index] / 256)] === 'undefined') {
+      const index = (y * imageData.width + x) * 4 * compressionRate
+      output += (colorSet[Math.floor(colorSet.length * (imageData.data[index] +
+                imageData.data[index + 1] + imageData.data[index + 2] +
+                imageData.data[index + 3]) / 4 / 256)]) // outputs an avarage of gbra pixels as translated string in colorset
+      if (typeof colorSet[Math.floor(colorSet.length * imageData.data[index] / 256)] === 'undefined') {
         break
       }
     }
